0974ab4d5f0a6396cbefc5f129453ee379ebe58d
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-03-22 18:31:20 +0000

    chaned dimensions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("perimeter-rectangle").innerHTML = 
+    "<p>The Perimeter is: = " + 2 * (5 + 3) + "cm. </p>"
+  document.getElementById("area-rectangle").innerHTML = 
+    "<p>The Area is: = " + 5 * 3 + "cm². </p>" 
+}
